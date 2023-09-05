73daa6f368f71515aa54bd1828e3f91e9d19ff83
helpimnotdrowning <35247379+helpimnotdrowning@users.noreply.github.com>
2023-09-05 01:32:54 -0500

    Make color codes more readable

--- dl.ps1
+++ dl.ps1
@@ -10,31 +10,34 @@ If (($args[0] -match "^(http(s)?:\/\/)?.*\.?twitch\.tv\/") -and -not ($args[0] -
     exit
 }
 
-If ((-not (Test-Path $(get-content "$PSScriptRoot/__cookies_file_path.txt") -PathType Leaf))) {
+If (-not (Test-Path $(get-content "$PSScriptRoot/__cookies_file_path.txt") -PathType Leaf)) {
     Write-Warning "Watch out! The cookies.txt file specified in $PSScriptRoot/__cookies_file_path.txt doesn't exist!"
     Write-Warning "Scripts `"(2) YouTube stream`" and `"(5) YouTube community posts`" will not capture paywalled or otherwise account-restricted content without this!"
 }
 
+$YEL = $PSStyle.Foreground.Yellow
+$000 = $PSStyle.Reset
+$GRY = $PSStyle.Foreground.BrightBlack
+
 Write-Host "What would you like to do with link $($args[0])? Choose [1->8]"
-Write-Host "($($PSStyle.Foreground.Yellow)1$($PSStyle.Reset)) $($PSStyle.Foreground.Yellow)STANDARD$($PSStyle.Reset) $($PSStyle.Foreground.BrightBlack)[standard-processed]$($PSStyle.Reset)"
+Write-Host "($YEL`1$000) $YEL`STANDARD$000 $GRY[standard-processed]$000"
 Write-Host "    - Video, metadata, subs for pretty much EVERYTHING"
-Write-Host "($($PSStyle.Foreground.Yellow)2$($PSStyle.Reset)) $($PSStyle.Foreground.Yellow)YouTube stream$($PSStyle.Reset) $($PSStyle.Foreground.BrightBlack)[stream-unprocessed]$($PSStyle.Reset)"
-Write-Host "    - Future or in-progress; video only, metadata if not privated"
-Write-Host "($($PSStyle.Foreground.Yellow)3$($PSStyle.Reset)) $($PSStyle.Foreground.Yellow)Twitch stream$($PSStyle.Reset) $($PSStyle.Foreground.BrightBlack)[twitch]$($PSStyle.Reset)"
-Write-Host "    - Past or present, future may work?; video and metadata"
-Write-Host "($($PSStyle.Foreground.Yellow)4$($PSStyle.Reset)) $($PSStyle.Foreground.Yellow)FULL$($PSStyle.Reset) $($PSStyle.Foreground.BrightBlack)[full]$($PSStyle.Reset)"
-Write-Host "    - Video, metadata, subs, comments, probably more I forgot, best on YouTube but working anywhere else"
-Write-Host "($($PSStyle.Foreground.Yellow)5$($PSStyle.Reset)) $($PSStyle.Foreground.Yellow)YouTube community posts$($PSStyle.Reset) $($PSStyle.Foreground.BrightBlack)[community-posts]$($PSStyle.Reset)"
-Write-Host "    - Public and Members, when joined"
-Write-Host "($($PSStyle.Foreground.Yellow)6$($PSStyle.Reset)) $($PSStyle.Foreground.Yellow)Video + Audio only$($PSStyle.Reset) $($PSStyle.Foreground.BrightBlack)[_video-audio]$($PSStyle.Reset)"
-Write-Host "    - MP4/AAC preferred, fallback to yt-dlp 'best' format"
-Write-Host "($($PSStyle.Foreground.Yellow)7$($PSStyle.Reset)) $($PSStyle.Foreground.Yellow)Subtitles + Live Chat only$($PSStyle.Reset) $($PSStyle.Foreground.BrightBlack)[_subs-chat]$($PSStyle.Reset)"
-Write-Host "    - Only real languages, no auto captions. Live chat may or may not be only for YouTube, idk"
-Write-Host "($($PSStyle.Foreground.Yellow)8$($PSStyle.Reset)) $($PSStyle.Foreground.Yellow)Metadata + Comments only$($PSStyle.Reset) $($PSStyle.Foreground.BrightBlack)[_info-json-comments]$($PSStyle.Reset)"
+Write-Host "($YEL`2$000) $YEL`YouTube stream$000 $GRY[stream-unprocessed]$000"
+Write-Host "    -`Future or in-progress; video only, metadata if not privated"
+Write-Host "($YEL`3$000) $YEL`Twitch stream$000 $GRY[twitch]$000"
+Write-Host "    -`Past or present, future may work?; video and metadata"
+Write-Host "($YEL`4$000) $YEL`FULL$000 $GRY[full]$000"
+Write-Host "    -`Video, metadata, subs, comments, probably more I forgot, best on YouTube but working anywhere else"
+Write-Host "($YEL`5$000) $YEL`YouTube community posts$000 $GRY[community-posts]$000"
+Write-Host "    -`Public and Members, when joined"
+Write-Host "($YEL`6$000) $YEL`Video + Audio only$000 $GRY[_video-audio]$000"
+Write-Host "    -`MP4/AAC preferred, fallback to yt-dlp 'best' format"
+Write-Host "($YEL`7$000) $YEL`Subtitles + Live Chat only$000 $GRY[_subs-chat]$000"
+Write-Host "    -`Only real languages, no auto captions. Live chat may or may not be only for YouTube, idk"
+Write-Host "($YEL`8$000) $YEL`Metadata + Comments only$000 $GRY[_info-json-comments]$000"
 Write-Host "    - Full metadata (info.json, description) and ALL comments present."
 
 $choice = Read-Host
-
 Switch ($choice) {
     1 {&"$PSScriptRoot/standard-processed" $args}
     2 {&"$PSScriptRoot/stream-unprocessed" $args}
